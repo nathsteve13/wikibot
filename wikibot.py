85adb2df9500d794d353a91352a51f8d2f39eaab
Nathanael Steven Soetrisno <116873291+nathsteve13@users.noreply.github.com>
2025-05-10 08:56:08 +0000

    test for cli

--- wikibot.py
+++ wikibot.py
@@ -1,7 +1,13 @@
-import wikipedia
+import click
+from mylib.bot import scrape
 
-def scrape(name="Microsoft", length=1):
-    result = wikipedia.summary(name, sentences=length)
-    return result
 
-print(scrape("Facebook", 3))
+@click.command()
+@click.option("--name", help="Web page we want to scrape")
+def cli(name):
+    result = scrape(name)
+    click.echo(click.style(f"{result}:", fg="blue"))
+
+
+if __name__ == "__main__":
+    cli()
